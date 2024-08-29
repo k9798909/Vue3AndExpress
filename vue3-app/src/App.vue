5824f72f1dcafabce391acf455a04016f2938056
k9798909 <k9798909@gmail.com>
2024-08-29 16:51:37 +0800

    side bar 增加縮放

--- vue3-app/src/App.vue
+++ vue3-app/src/App.vue
@@ -14,7 +14,13 @@ const drawer = ref(false)
     <v-navigation-drawer v-model="drawer">
       <v-list>
         <v-list-item title="Home" to="/"></v-list-item>
-        <v-list-item title="TodoItem" to="/todoItem"></v-list-item>
+
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" title="功能"></v-list-item>
+          </template>
+          <v-list-item to="/todoItem">待辦清單</v-list-item>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
 
